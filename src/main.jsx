18cfce8be58b9301e68a26e9252a03436d23f226
nicolas-katz <83619866+nicolas-katz@users.noreply.github.com>
2022-10-18 21:51:49 -0300

    solving r-r-d gh-p problem

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { StoreContextProvider } from './context/StoreContext'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <StoreContextProvider>
         <App />
       </StoreContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
